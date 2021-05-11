ba640dedd5a9f0cfec5a92dd9bb4d711852e8112
treilly0607 <60261903+treilly0607@users.noreply.github.com>
2021-05-11 09:38:37 +0100

    Added Game Class along with its properties.

---
+++ TristanReilly_S00199625/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TristanReilly_S00199625
+{
+    class Game
+    {
+        // Properities for Game class
+        public string Name { get; set; }
+        public int MetacriticScore { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public string Game_Image { get; set; }
+
+    }       
+}
