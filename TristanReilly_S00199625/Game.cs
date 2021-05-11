62e0fc35e979152ac43da5bc09de62f5d5bd5ff4
treilly0607 <60261903+treilly0607@users.noreply.github.com>
2021-05-11 09:42:39 +0100

    Added decrease price method.

--- TristanReilly_S00199625/Game.cs
+++ TristanReilly_S00199625/Game.cs
@@ -12,8 +12,12 @@ namespace TristanReilly_S00199625
         public string Name { get; set; }
         public int MetacriticScore { get; set; }
         public string Description { get; set; }
-        public string Price { get; set; }
+        public int Price { get; set; }
         public string Game_Image { get; set; }
 
+        public void DecreasePrice(int value) // Decrease the price of a game by inputted amount 
+        {
+            Price = Price - value;
+        }
     }       
 }
